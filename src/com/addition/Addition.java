3c14902e87deb9df1b07732eecba97524a9d4c24
kru-nal <krunaal.java@gmail.com>
2025-05-06 13:55:45 +0530

    Adding Addition class

---
+++ src/com/addition/Addition.java
@@ -0,0 +1,12 @@
+package com.addition;
+
+public class Addition {
+public static void main(String[] args) {
+	int x =10;
+   double y =10.5;
+   
+   // Addition of two number is 
+   System.out.println((x+y));
+	
+}
+}
