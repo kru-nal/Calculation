4c6216cbaa292790e258840f98e9ff90f3a5a21d
kru-nal <krunaal.java@gmail.com>
2025-05-09 21:51:45 +0530

    add new class overloading

---
+++ src/com/addition/Overlloading.java
@@ -0,0 +1,16 @@
+package com.addition;
+
+public class Overlloading {
+	
+	public static void m1(int a ,String b) {
+		a =10;
+		b="krunal";
+		System.out.println(a+" "+b);
+	}
+	
+	public static void main(String[] args) {
+		m1(0, null);
+		
+	    }
+
+    }
