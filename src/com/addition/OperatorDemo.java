f89b54db0cbcc10ac4c47e3799811c76eb302a5d
kru-nal <krunaal.java@gmail.com>
2025-05-11 21:57:09 +0530

    class name change as operator

---
+++ src/com/addition/OperatorDemo.java
@@ -0,0 +1,16 @@
+package com.addition;
+
+public class OperatorDemo {
+	
+	public static void main(String []args) { 
+	// logical And operator demo &&
+	int a =5;
+	int b =10;
+	int c =8;
+	
+	System.out.println((a<b) && (b>c) ); // true 
+	System.out.println((a>c &&   c<b)); // false
+	   }
+
+
+    }
