7b283b660a31eff72baf1e00d64b37b550a3844f
kru-nal <krunaal.java@gmail.com>
2025-05-16 14:42:36 +0530

    Update OperatorDemo.java
    change D value as 50

--- src/com/addition/OperatorDemo.java
+++ src/com/addition/OperatorDemo.java
@@ -7,7 +7,7 @@ public class OperatorDemo {
 	int a =5;
 	int b =10;
 	int c =8;
-    int d = 25;
+    int d = 50;
 	
 	System.out.println((a<b) && (b>c) ); // true 
 	System.out.println((a>c &&   c<b)); // false
