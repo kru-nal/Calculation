446da9a7bf26dd96d9917fe85f4147f960e993f2
kru-nal <krunaal.java@gmail.com>
2025-05-16 14:31:10 +0530

    Update Addition.java
    make change bt Dev-2

--- src/com/addition/Addition.java
+++ src/com/addition/Addition.java
@@ -7,7 +7,7 @@ public static void main(String[] args) {
 	for (int i =1; i<=20; i++) {
 		if (i%2!=0) {
 			System.out.println(i);
-			
+			System.out.println("changes by dev 2 on remote repo);
 			i++;
 			
 		}
