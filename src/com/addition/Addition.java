5322470815d3c82e10da41b7dd3605eb48654e21
kru-nal <krunaal.java@gmail.com>
2025-05-16 14:45:40 +0530

    Add new Method

--- src/com/addition/Addition.java
+++ src/com/addition/Addition.java
@@ -1,20 +1,25 @@
 package com.addition;
 
 public class Addition {
+	
+	public static void m1() {
+		
+		System.out.println(" Add new method as M1");
+		
+	}
 public static void main(String[] args) {
 	// print number using loop.  odd number.1-20
 	
 	for (int i =1; i<=20; i++) {
 		if (i%2!=0) {
 			System.out.println(i);
-			
-
+		
 			i++;
-			
-			
+		
 		}
 	
 	}
 	System.out.println("add new statement form Dev-1 on local repo ");
+	m1();
                      }
      }
